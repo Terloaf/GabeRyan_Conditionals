621155504a1de5b2cf2e7c0436661ed3ad093a4a
Terloaf <greenyryan2501@gmail.com>
2025-09-15 10:24:49 -0300

    Initial commit

---
+++ GabeRyan_Conditionals/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GabeRyan_Conditionals
+{
+
+    enum WeaponType
+    {
+        pistol = 0,
+        sword = 1,
+        hammer = 2,
+        axe = 3
+    }
+       
+
+
+
+
+
+    internal class Program
+    {
+        static int playerHealth = 100;
+        static WeaponType weapon = WeaponType.pistol;
+
+
+        static void DealDamge(WeaponType weapontype)
+        {
+
+            if(weapontype == WeaponType.pistol)
+            {
+
+            }
+            else if (weapontype == WeaponType.sword)
+            {
+
+            }
+            else if (weapontype == WeaponType.hammer)
+            {
+
+            }
+            else if (weapontype == WeaponType.axe)
+            {
+
+            }
+            else
+            {
+
+            }
+
+
+        }
+
+        static void ChangeWeapon(WeaponType NewWeapon)
+        {
+            weapon = NewWeapon;
+        }
+
+        static void TakeDamage(int damage)
+        {
+            playerHealth -= damage;
+        }
+
+        static void Heal(int hp)
+        {
+            playerHealth += hp;
+        }
+
+   
+        static void ClearLine()
+        {
+            Console.ReadKey();
+            Console.Clear();
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine 
+        }
+
+      
+            
+
+
+
+       
+        
+         
+    }     
+}
+
+       
---
+++ GabeRyan_Conditionals/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("GabeRyan_Conditionals")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("NSCC")]
+[assembly: AssemblyProduct("GabeRyan_Conditionals")]
+[assembly: AssemblyCopyright("Copyright © NSCC 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b15ba28c-d5f1-45dc-b251-2fb10b8046c3")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
